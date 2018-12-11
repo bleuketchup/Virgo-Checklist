847d0a59ccccfbef66ed5fb3c2472e440af6a117
Darlene <darlene@darlenes-macbookpro2.local>
2018-12-11 13:03:07 -0800

    Initial Commit

---
+++ Virgo-Checklist/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Virgo-Checklist
+//
+//  Created by Darlene on 12/11/18.
+//  Copyright © 2018 Darlene Austria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
